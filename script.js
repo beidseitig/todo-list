8438d46a7944032aeb3660db2a349a50c7a8c990
RamosFonseca <beidseitig@gmail.com>
2021-12-20 04:04:21 -0300

    Adicao de outra section no requisito 2 para melhorar a organizacao e requisito 5

--- script.js
+++ script.js
@@ -13,21 +13,39 @@ const main = document.createElement('main');
 main.setAttribute('id', 'main');
 document.body.appendChild(main);
 
+const creationSection = document.createElement('section');
+creationSection.setAttribute('id', 'creation');
+main.appendChild(creationSection);
+
 const paragraph = document.createElement('p');
 paragraph.setAttribute('id', 'funcionamento');
 paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
-main.appendChild(paragraph);
+creationSection.appendChild(paragraph);
 
 //Requisito 3
 const input = document.createElement('input');
 input.setAttribute('id', 'texto-tarefa');
-main.appendChild(input);
+creationSection.appendChild(input);
 
 //Requisito 4
 const listSection = document.createElement('section');
-listSection.setAttribute('id', 'listSection');
+listSection.setAttribute('id', 'list');
 main.appendChild(listSection);
 
 const list = document.createElement('ol');
 list.setAttribute('id', 'lista-tarefas');
 listSection.appendChild(list);
+
+//Requisito 5
+const button = document.createElement('button');
+button.setAttribute('id', 'criar-tarefa');
+button.innerText = 'Adicionar';
+creationSection.appendChild(button);
+
+button.addEventListener('click', function () {
+    let item = document.createElement('li');
+    item.setAttribute('class', 'itens');
+    item.innerText = input.value;
+    list.appendChild(item);
+    input.value = '';
+});
