579447fc356566cfc9e626a8f05036d448d8223b
RamosFonseca <beidseitig@gmail.com>
2021-12-20 03:27:20 -0300

    Requisito 3 e melhor divisao

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Requisito 1
 const header = document.createElement('header');
 header.setAttribute('id', 'header');
 document.body.appendChild(header);
@@ -7,6 +8,7 @@ h1.setAttribute('id', 'title');
 h1.innerText = 'Minha Lista de Tarefas';
 header.appendChild(h1);
 
+//Requisito 2
 const main = document.createElement('main');
 main.setAttribute('id', 'main');
 document.body.appendChild(main);
@@ -15,3 +17,8 @@ const funcionamento = document.createElement('p');
 funcionamento.setAttribute('id', 'funcionamento');
 funcionamento.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 main.appendChild(funcionamento);
+
+//Requisito 3
+const input = document.createElement('input');
+input.setAttribute('id', 'texto-tarefa');
+main.appendChild(input);
