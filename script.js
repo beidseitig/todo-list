2e5346ad84920e1763aced38ad1c55174660824a
RamosFonseca <beidseitig@gmail.com>
2021-12-20 03:12:24 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const header = document.createElement('header');
 header.setAttribute('id', 'title');
 document.body.appendChild(header);
 header.innerText = 'Minha Lista de Tarefas';
+
+const main = document.createElement('main');
+main.setAttribute('id', 'main');
+document.body.appendChild(main);
+
+const funcionamento = document.createElement('p');
+funcionamento.setAttribute('id', 'funcionamento');
+document.main.appendChild(funcionamento);
+funcionamento.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
